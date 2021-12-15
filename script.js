d48d1dbe8078d2057f182e1c97f03c993d3146f9
unknown <guerrerojorgef@gmail.com>
2021-12-14 22:42:08 -0300

    Final Assigment Done

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
+
+
 function compute()
 {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
     p = document.getElementById("principal").value;
-    
+
+    if (p < "0") {
+        alert("Amount must be grater than 0");
+        document.getElementById("result").innerHTML = "";
+    } else{
+        document.getElementById("result").innerHTML = "<br/>" +
+            "If you deposit: " + principal + "<br/>" +
+            "at an interest rate of: " + rate + "<br/>" +
+            "You will receive an amount of: " + interest + "<br/>" +
+            "in the year: " + year;
+    }
 }
-        
+
+
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
+
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
